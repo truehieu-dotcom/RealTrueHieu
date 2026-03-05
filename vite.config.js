e5f14f8787a1c164683293f562a903ffca984012
truehieu-dotcom <trung.minhhieutran@gmail.com>
2026-03-06 00:05:54 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Quan trọng: định nghĩa root
-  root: '.',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  }
 })
